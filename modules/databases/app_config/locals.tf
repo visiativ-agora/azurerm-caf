967f0b454ced63633cf665f47f07acdee0baa0b9
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-31 16:15:46 +0200

    fix

--- modules/databases/app_config/locals.tf
+++ modules/databases/app_config/locals.tf
@@ -2,8 +2,6 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-
-
   tags = var.base_tags ? merge(
     var.global_settings.tags,
     try(var.resource_group.tags, null),
@@ -17,4 +15,12 @@ locals {
 
   location            = coalesce(var.location, var.resource_group.location)
   resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
+}
+
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
+  }
 }
--- modules/databases/app_config/variables.tf
+++ modules/databases/app_config/variables.tf
@@ -1,46 +1,21 @@
-variable "settings" {
-  description = "The settings for the Azure resource."
-  type        = any
-  # For the future
-  /*type = object({
-    sku_name = optional(string)
-    local_auth_enabled = optional(bool)
-    public_network_access = optional(bool)
-    identity = optional(object({
-      type = string
-    }))
-    tags = optional(map(string))
-    private_endpoints = optional(map(object({
-      name = string
-      lz_key = optional(string)
-      vnet_key = string
-      subnet_key = string
-      subnet_id = optional(string)
-    })))
-    dynamic_settings = optional(map(map(map(object({
-      lz_key = optional(string)
-      attribute_key = string
-    })))))
-  })*/
-}
+variable "keyvaults" {}
+variable "resource_groups" {}
+
+variable "settings" {}
 
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
-  type        = any
 }
 variable "location" {
   description = "location of the resource if different from the resource group."
-  type        = string
   default     = null
 }
 variable "resource_group_name" {
-  description = "Resource group object to deploy the Azure resource"
-  type        = string
+  description = "Resource group object to deploy the virtual machine"
   default     = null
 }
 variable "resource_group" {
-  description = "Resource group object to deploy the Azure resource"
-  type        = any
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
@@ -49,29 +24,17 @@ variable "base_tags" {
 
 variable "name" {
   description = "(Required) Name of the App Config"
-  type        = string
 }
 
 variable "combined_objects" {
-  description = "Combined objects for the resource."
-  type        = any
-  default     = {}
+  default = {}
 }
 
-variable "client_config" {
-  description = "Client configuration object (see module README.md)."
-  type        = any
-}
+variable "client_config" {}
 variable "vnets" {
-  description = "Virtual networks for the resource."
-  type        = map(any)
-  default     = {}
+  default = {}
 }
 variable "private_dns" {
-  description = "Private DNS settings for the resource."
-  type        = map(any)
-  default     = {}
+  default = {}
 }
 variable "managed_identities" {}
-variable "keyvaults" {}
-variable "resource_groups" {}
