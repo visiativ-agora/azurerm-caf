9b9b47cd212fed19ea851618ff56589b0a76d0d5
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-31 15:47:25 +0200

    fix compute

--- modules/compute/container_app/variables.tf
+++ modules/compute/container_app/variables.tf
@@ -2,22 +2,36 @@ variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = bool
 }
-variable "client_config" {}
+variable "client_config" {
+  description = "Client configuration object (see module README.md)."
+  type        = any
+}
 variable "diagnostics" {}
 variable "diagnostic_profiles" {}
 variable "combined_diagnostics" {}
 variable "combined_resources" {
   description = "Provide a map of combined resources for environment_variables_from_resources"
   default     = {}
+  type        = any
 }
 variable "global_settings" {}
-variable "location" {}
 variable "resource_group_name" {}
 variable "resource_group" {
-  description = "Resource group object to deploy the virtual machine"
+  description = "Resource group object to deploy the Azure resource"
+  type        = any
 }
-variable "settings" {}
+variable "settings" {
+  description = "The settings for the Azure resource."
+  type        = any
+}
 variable "container_app_environment_id" {}
+
+
+variable "remote_objects" {
+  description = "Remote objects for dependencies."
+  type        = any
+  default     = {}
+}
 variable "workload_profile_name" {
   description = "The name of the Workload Profile in the Container App Environment that this Container App should be placed in."
   default     = "Consumption"
