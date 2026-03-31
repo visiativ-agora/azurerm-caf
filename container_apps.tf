1aa2e74deab091701279f4a112bf87082533c2a0
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-03-31 16:02:41 +0200

    fix container app

--- container_apps.tf
+++ container_apps.tf
@@ -2,7 +2,6 @@ module "container_apps" {
   source   = "./modules/compute/container_app"
   for_each = local.compute.container_apps
 
-  location                     = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group               = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name          = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   base_tags                    = local.global_settings.inherit_tags
