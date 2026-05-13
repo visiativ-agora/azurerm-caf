21d320ef87d826765087aba32959d6bf5496d8eb
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-05-13 11:00:30 +0200

    refactor: enhance revoke database permissions script with dynamic environment variables

--- modules/databases/postgresql_flexible_server/db_permissions.tf
+++ modules/databases/postgresql_flexible_server/db_permissions.tf
@@ -5,6 +5,9 @@ resource "null_resource" "set_db_permissions" {
     db_usernames = join(",", each.value.db_usernames)
     db_roles     = join(",", each.value.db_roles)
     database     = each.value.database
+    server_fqdn  = local.server_fqdn
+    admin_user   = try(var.settings.administrator_username, "pgadmin")
+    admin_pwd    = try(azurerm_postgresql_flexible_server.postgresql.administrator_password, data.azurerm_key_vault_secret.postgresql_admin_password[0].value)
   }
 
   provisioner "local-exec" {
@@ -20,6 +23,21 @@ resource "null_resource" "set_db_permissions" {
       DBUSERNAMES  = format("'%s'", join(",", each.value.db_usernames))
       DBROLES      = format("'%s'", join(",", each.value.db_roles))
       SQLFILEPATH  = format("%s/scripts/set_db_permissions.sql", path.module)
+    }
+  }
+
+  provisioner "local-exec" {
+    when        = destroy
+    command     = format("%s/scripts/revoke_db_permissions.sh", path.module)
+    interpreter = ["/bin/bash"]
+    on_failure  = continue
+    environment = {
+      PGHOST      = self.triggers.server_fqdn
+      PGPORT      = "5432"
+      PGDATABASE  = self.triggers.database
+      DBADMINUSER = self.triggers.admin_user
+      DBADMINPWD  = self.triggers.admin_pwd
+      DBUSERNAMES = self.triggers.db_usernames
     }
   }
 }
