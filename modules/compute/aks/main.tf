86f8d1f679c6a4f8b8b9906fec41aa9aab97b097
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-06-09 17:57:14 +0200

    Add terraform block with required providers and version constraints in AKS module

--- modules/compute/aks/main.tf
+++ modules/compute/aks/main.tf
@@ -1,1 +1,13 @@
-# Review other files in the same directory for more details on how to use the module
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = ">= 1.0.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+  }
+}
