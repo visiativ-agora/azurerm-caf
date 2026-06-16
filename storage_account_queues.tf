43c154bb57e5c668e7791531d943d6550ea79dae
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-06-16 14:11:08 +0200

    fix: update storage_account_queues module to use storage_account_id instead of storage_account_name

--- storage_account_queues.tf
+++ storage_account_queues.tf
@@ -1,7 +1,7 @@
 module "storage_account_queues" {
   source               = "./modules/storage_account/queue"
   for_each             = local.storage.storage_account_queues
-  storage_account_name = module.storage_accounts[each.value.storage_account_key].name
+  storage_account_id   = module.storage_accounts[each.value.storage_account_key].id
   settings             = each.value
 
 }
