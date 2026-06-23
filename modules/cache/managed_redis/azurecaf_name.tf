8e70aabfc609650e860240794974f6646f0527ed
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-06-23 10:01:44 +0200

    fix

--- modules/cache/managed_redis/azurecaf_name.tf
+++ modules/cache/managed_redis/azurecaf_name.tf
@@ -2,7 +2,7 @@
 # Note: azurecaf currently has no dedicated resource_type for managed redis; using azurerm_redis_cache resourcetype via azurecaf_name.
 
 resource "azurecaf_name" "managed_redis" {
-  name          = var.settings.redis.name
+  name          = var.settings.name
   resource_type = "azurerm_redis_cache"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
