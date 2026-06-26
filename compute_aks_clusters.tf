210b28d0935c41d5aeb5232c9064509884338fb0
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-06-26 17:43:14 +0200

    fix: update keys for private DNS zone and application gateway in aks_clusters module

--- compute_aks_clusters.tf
+++ compute_aks_clusters.tf
@@ -28,10 +28,10 @@ module "aks_clusters" {
 
   remote_objects = {
     #subnet_id           = can(each.value.vnet.subnet_key) ? local.combined_objects_networking[try(each.value.vnet.lz_key, local.client_config.landingzone_key)][each.value.vnet.key].subnets[each.value.vnet.subnet_key].id : null
-    private_dns_zone_id    = can(each.value.settings.private_dns_zone.key) ? local.combined_objects_private_dns[try(each.value.settings.private_dns_zone.lz_key, local.client_config.landingzone_key)][each.value.settings.private_dns_zone.key].id : null
-    application_gateway_id = can(each.value.settings.ingress_application_gateway.key) ? local.combined_objects_application_gateway_platforms[try(each.value.settings.ingress_application_gateway.lz_key, local.client_config.landingzone_key)][each.value.settings.ingress_application_gateway.key] : null
-    pod_subnet_id          = can(each.value.settings.default_node_pool.pod_subnet_key) ? local.combined_objects_networking[try(each.value.settings.lz_key, var.client_config.landingzone_key)][each.value.settings.vnet_key].subnets[try(each.value.settings.default_node_pool.pod_subnet_key, each.value.settings.default_node_pool.pod_subnet.key)].id : null
-    vnet_subnet_id         = can(each.value.settings.default_node_pool.subnet_key) ? local.combined_objects_networking[try(each.value.settings.vnet.lz_key, each.value.settings.lz_key, var.client_config.landingzone_key)][try(each.value.settings.vnet.key, each.value.settings.vnet_key)].subnets[try(each.value.settings.default_node_pool.subnet_key, each.value.settings.default_node_pool.subnet.key)].id : null
+    private_dns_zone_id    = can(each.value.private_dns_zone.key) ? local.combined_objects_private_dns[try(each.value.private_dns_zone.lz_key, local.client_config.landingzone_key)][each.value.private_dns_zone.key].id : null
+    application_gateway_id = can(each.value.ingress_application_gateway.key) ? local.combined_objects_application_gateway_platforms[try(each.value.ingress_application_gateway.lz_key, local.client_config.landingzone_key)][each.value.ingress_application_gateway.key] : null
+    pod_subnet_id          = can(each.value.default_node_pool.pod_subnet_key) ? local.combined_objects_networking[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[try(each.value.default_node_pool.pod_subnet_key, each.value.default_node_pool.pod_subnet.key)].id : null
+    vnet_subnet_id         = can(each.value.default_node_pool.subnet_key) ? local.combined_objects_networking[try(each.value.vnet.lz_key, each.value.lz_key, var.client_config.landingzone_key)][try(each.value.vnet.key, each.value.vnet_key)].subnets[try(each.value.default_node_pool.subnet_key, each.value.default_node_pool.subnet.key)].id : null
     diagnostics            = local.combined_diagnostics
     managed_identities     = local.combined_objects_managed_identities
     vnets                  = local.combined_objects_networking
