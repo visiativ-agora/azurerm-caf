8900b36c141209eaea2fddbb4d464bb8feb42b40
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-15 15:43:54 +0200

    fix: update license_type assignment to use null instead of "None" in dedicated host module

--- modules/compute/dedicated_hosts/module.tf
+++ modules/compute/dedicated_hosts/module.tf
@@ -18,6 +18,6 @@ resource "azurerm_dedicated_host" "dh" {
   sku_name                = var.settings.sku_name
   platform_fault_domain   = var.settings.platform_fault_domain
   auto_replace_on_failure = try(var.settings.auto_replace_on_failure, true)
-  license_type            = try(var.settings.license_type, "None")
+  license_type            = try(var.settings.license_type, null)
   tags                    = local.tags
 }
