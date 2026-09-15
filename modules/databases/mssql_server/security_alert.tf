5643ecd776f104bf60671ecfb9a10eeeda740ec5
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-15 15:45:22 +0200

    fix: update email_account_admins to email_account_admins_enabled in MSSQL server security alert policy

--- modules/databases/mssql_server/security_alert.tf
+++ modules/databases/mssql_server/security_alert.tf
@@ -15,13 +15,13 @@ data "azurerm_storage_account" "mssql_security_alert" {
 resource "azurerm_mssql_server_security_alert_policy" "mssql" {
   count = try(var.settings.security_alert_policy, null) == null ? 0 : 1
 
-  resource_group_name        = local.resource_group_name
-  server_name                = azurerm_mssql_server.mssql.name
-  state                      = try(var.settings.state, "Enabled")
-  storage_endpoint           = data.azurerm_storage_account.mssql_security_alert.0.primary_blob_endpoint
-  storage_account_access_key = data.azurerm_storage_account.mssql_security_alert.0.primary_access_key
-  disabled_alerts            = try(var.settings.disabled_alerts, null)
-  email_account_admins       = try(var.settings.email_subscription_admins, false)
-  email_addresses            = try(var.settings.email_addresses, null)
-  retention_days             = try(var.settings.retention_days, 0)
+  resource_group_name          = local.resource_group_name
+  server_name                  = azurerm_mssql_server.mssql.name
+  state                        = try(var.settings.state, "Enabled")
+  storage_endpoint             = data.azurerm_storage_account.mssql_security_alert.0.primary_blob_endpoint
+  storage_account_access_key   = data.azurerm_storage_account.mssql_security_alert.0.primary_access_key
+  disabled_alerts              = try(var.settings.disabled_alerts, null)
+  email_account_admins_enabled = try(var.settings.email_account_admins_enabled, false)
+  email_addresses              = try(var.settings.email_addresses, null)
+  retention_days               = try(var.settings.retention_days, 0)
 }
