d5e753aa47d6239e736d600e9901985c3b8ba88d
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-15 15:45:30 +0200

    fix: update recommendations_enabled to recommendations in IoT security solution configuration

--- modules/iot/security/security_solution/security_solution.tf
+++ modules/iot/security/security_solution/security_solution.tf
@@ -31,25 +31,25 @@ resource "azurerm_iot_security_solution" "securitysolution" {
   log_analytics_workspace_id = try(var.settings.log_analytics_workspace_id, null)
   log_unmasked_ips_enabled   = try(var.settings.log_unmasked_ips_enabled, null)
 
-  dynamic "recommendations_enabled" {
-    for_each = lookup(var.settings, "recommendations_enabled", {}) == {} ? [] : [1]
+  dynamic "recommendations" {
+    for_each = lookup(var.settings, "recommendations", {}) == {} ? [] : [1]
     content {
-      acr_authentication               = try(var.settings.recommendations_enabled.acr_authentication, null)
-      agent_send_unutilized_msg        = try(var.settings.recommendations_enabled.agent_send_unutilized_msg, null)
-      baseline                         = try(var.settings.recommendations_enabled.baseline, null)
-      edge_hub_mem_optimize            = try(var.settings.recommendations_enabled.edge_hub_mem_optimize, null)
-      edge_logging_option              = try(var.settings.recommendations_enabled.edge_logging_option, null)
-      inconsistent_module_settings     = try(var.settings.recommendations_enabled.inconsistent_module_settings, null)
-      install_agent                    = try(var.settings.recommendations_enabled.install_agent, null)
-      ip_filter_deny_all               = try(var.settings.recommendations_enabled.ip_filter_deny_all, null)
-      ip_filter_permissive_rule        = try(var.settings.recommendations_enabled.ip_filter_permissive_rule, null)
-      open_ports                       = try(var.settings.recommendations_enabled.open_ports, null)
-      permissive_firewall_policy       = try(var.settings.recommendations_enabled.permissive_firewall_policy, null)
-      permissive_input_firewall_rules  = try(var.settings.recommendations_enabled.permissive_input_firewall_rules, null)
-      permissive_output_firewall_rules = try(var.settings.recommendations_enabled.permissive_output_firewall_rules, null)
-      privileged_docker_options        = try(var.settings.recommendations_enabled.privileged_docker_options, null)
-      shared_credentials               = try(var.settings.recommendations_enabled.shared_credentials, null)
-      vulnerable_tls_cipher_suite      = try(var.settings.recommendations_enabled.vulnerable_tls_cipher_suite, null)
+      acr_authentication               = try(var.settings.recommendations.acr_authentication, null)
+      agent_send_unutilized_msg        = try(var.settings.recommendations.agent_send_unutilized_msg, null)
+      baseline                         = try(var.settings.recommendations.baseline, null)
+      edge_hub_mem_optimize            = try(var.settings.recommendations.edge_hub_mem_optimize, null)
+      edge_logging_option              = try(var.settings.recommendations.edge_logging_option, null)
+      inconsistent_module_settings     = try(var.settings.recommendations.inconsistent_module_settings, null)
+      install_agent                    = try(var.settings.recommendations.install_agent, null)
+      ip_filter_deny_all               = try(var.settings.recommendations.ip_filter_deny_all, null)
+      ip_filter_permissive_rule        = try(var.settings.recommendations.ip_filter_permissive_rule, null)
+      open_ports                       = try(var.settings.recommendations.open_ports, null)
+      permissive_firewall_policy       = try(var.settings.recommendations.permissive_firewall_policy, null)
+      permissive_input_firewall_rules  = try(var.settings.recommendations.permissive_input_firewall_rules, null)
+      permissive_output_firewall_rules = try(var.settings.recommendations.permissive_output_firewall_rules, null)
+      privileged_docker_options        = try(var.settings.recommendations.privileged_docker_options, null)
+      shared_credentials               = try(var.settings.recommendations.shared_credentials, null)
+      vulnerable_tls_cipher_suite      = try(var.settings.recommendations.vulnerable_tls_cipher_suite, null)
     }
   }
 
