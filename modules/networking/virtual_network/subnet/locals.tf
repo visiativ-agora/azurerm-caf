c1d51eeb6043e49e771450dcce3d1115f2f16877
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-18 14:39:00 +0200

    fix: refactor service_endpoint_input handling for improved clarity

--- modules/networking/virtual_network/subnet/locals.tf
+++ modules/networking/virtual_network/subnet/locals.tf
@@ -21,8 +21,13 @@ locals {
       service            = service
       network_identifier = null
     }
-    ] : [
-    for endpoint in(can(local.service_endpoint_input.service) ? [local.service_endpoint_input] : local.service_endpoint_input) : {
+  ] : can(local.service_endpoint_input.service) ? [
+    {
+      service            = local.service_endpoint_input.service
+      network_identifier = try(local.service_endpoint_input.network_identifier, null)
+    }
+  ] : [
+    for endpoint in local.service_endpoint_input : {
       service            = try(endpoint.service, endpoint)
       network_identifier = try(endpoint.network_identifier, null)
     }
