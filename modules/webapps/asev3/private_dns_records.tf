3be5c53215047a2403a29ff176ef9dd471f9f726
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-18 09:43:27 +0200

    fix: add private_dns_zone_id to azurerm_private_dns_a_record resource

--- modules/webapps/asev3/private_dns_records.tf
+++ modules/webapps/asev3/private_dns_records.tf
@@ -2,9 +2,10 @@ resource "azurerm_private_dns_a_record" "a_records" {
   depends_on = [azurerm_app_service_environment_v3.asev3]
   for_each   = try(var.settings.private_dns_records.a_records, {})
 
-  name    = each.value.name == "" ? azurecaf_name.asev3.result : format("%s.%s", each.value.name, azurecaf_name.asev3.result)
-  ttl     = each.value.ttl
-  records = azurerm_app_service_environment_v3.asev3.internal_inbound_ip_addresses
-  tags    = merge(local.tags, try(each.value.tags, {}))
+  name                = each.value.name == "" ? azurecaf_name.asev3.result : format("%s.%s", each.value.name, azurecaf_name.asev3.result)
+  private_dns_zone_id = try(var.private_dns[each.value.lz_key][each.value.private_dns_key].name, var.private_dns[var.client_config.landingzone_key][each.value.private_dns_key].id)
+  ttl                 = each.value.ttl
+  records             = azurerm_app_service_environment_v3.asev3.internal_inbound_ip_addresses
+  tags                = merge(local.tags, try(each.value.tags, {}))
 }
 
