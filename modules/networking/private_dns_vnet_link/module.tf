bb49ce6a33993c4c54e5f8f32b28a4d810dc3be3
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-18 09:43:17 +0200

    fix: update azurerm provider version to 5.0.0 and adjust private_dns_zone_virtual_network_link resource attributes

--- modules/networking/private_dns_vnet_link/module.tf
+++ modules/networking/private_dns_vnet_link/module.tf
@@ -13,10 +13,9 @@ resource "azurecaf_name" "pnetlk" {
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_links" {
   for_each = var.settings.private_dns_zones
 
-  name                  = azurecaf_name.pnetlk[each.key].result
-  resource_group_name   = can(each.value.resource_group_name) ? each.value.resource_group_name : var.private_dns[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].resource_group_name
-  private_dns_zone_name = can(each.value.private_dns_zone_name) ? each.value.private_dns_zone_name : var.private_dns[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].name
-  virtual_network_id    = var.virtual_network_id
-  registration_enabled  = try(each.value.registration_enabled, false)
-  tags                  = merge(local.tags, try(each.value.tags, null))
+  name                 = azurecaf_name.pnetlk[each.key].result
+  private_dns_zone_id  = can(each.value.private_dns_zone_id) ? each.value.private_dns_zone_id : var.private_dns[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].id
+  virtual_network_id   = var.virtual_network_id
+  registration_enabled = try(each.value.registration_enabled, false)
+  tags                 = merge(local.tags, try(each.value.tags, null))
 }
