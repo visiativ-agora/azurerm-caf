2a9dd6c34e6643251613c1ba4b57ada4830a5bcb
Kévin DELMONT <kevin.delmont@visiativ.com>
2026-09-18 09:41:54 +0200

    fix: rename cluster_resource_id variable to cluster_id for consistency

--- modules/databases/data_explorer/kusto_attached_database_configurations/variables.tf
+++ modules/databases/data_explorer/kusto_attached_database_configurations/variables.tf
@@ -20,7 +20,7 @@ variable "settings" {
 variable "cluster_name" {
   description = "(Required) Specifies the name of the Kusto Cluster for which the configuration will be created. Changing this forces a new resource to be created."
 }
-variable "cluster_resource_id" {
+variable "cluster_id" {
   description = " (Required) The resource id of the cluster where the databases you would like to attach reside."
 }
 variable "database_name" {
